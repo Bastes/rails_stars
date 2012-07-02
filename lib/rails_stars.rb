687ca26b180a652904f578e3092d7aefa047ffde
Michel Belleville <mib@octo.com>
2012-07-02 11:10:31 +0200

    Needs an engine to require stuff and added more gems for presentation

--- lib/rails_stars.rb
+++ lib/rails_stars.rb
@@ -1,6 +1,7 @@
 require 'rails_stars/acts_as_star_receiver'
 require 'rails_stars/acts_as_star_giver'
 require 'rails_stars/helper'
+require 'rails_stars/engine'
 
 module RailsStars
   autoload :Star, 'rails_stars/star'
---
+++ lib/rails_stars/engine.rb
@@ -0,0 +1,9 @@
+module RailsStars
+  class Engine < ::Rails::Engine
+    require 'rails'
+    require 'jquery-rails'
+    require 'haml-rails'
+    require 'sass-rails'
+    require 'bourbon'
+  end
+end
